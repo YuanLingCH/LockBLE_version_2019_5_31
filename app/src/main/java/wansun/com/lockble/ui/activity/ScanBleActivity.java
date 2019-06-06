958be8dc26fc59a409d22afa7287d247ff6d3161
yuanling <yuanling@cnwansun.com>
2019-06-06 18:42:06 +0800

    添加了判断

--- app/src/main/java/wansun/com/lockble/ui/activity/ScanBleActivity.java
+++ app/src/main/java/wansun/com/lockble/ui/activity/ScanBleActivity.java
@@ -96,6 +96,8 @@ public class ScanBleActivity extends BaseActivity implements AdapterView.OnItemC
                     tv_scan_ble.setText("蓝牙模块返回数据："+s );
                     if ("CC".equals(s)){
                         startActivity(new Intent(ScanBleActivity.this,WelcomeActivity.class));
+                    }else {
+                        Toast.makeText(ScanBleActivity.this, "蓝牙连接失败，请重新连接", Toast.LENGTH_SHORT).show();
                     }
 
                 }
